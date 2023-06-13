55ed548b591f8f5ab023767dc245eea502537841
konishi333 <jobhuntingkonishi3@gmail.com>
2023-06-13 14:30:21 +0900

    CSVファイルへの書き込み処理追加
    csv-writerをインストールしています

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = 8000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const app = express();
 
@@ -18,8 +19,22 @@ axios(URL).then((response) => {
     const title = $(this).find(".title-link--3Ho6z").text();
     const price = $(this).find(".price--OX_YW").text();
     data.push({ title, price});
-    console.log(data);
+    // console.log(data);
   });
+
+  //CSVへの書き込み
+  const csvWriter = createCsvWriter({
+    path: "data.csv",
+    header: [
+      { id: "title", title: "Title"},
+      { id: "price", title: "Price"},
+    ]
+  });
+
+  csvWriter
+    .writeRecords(data)
+    .then(() => console.log("CSV file successfully written."));
+
 }).catch(error => console.log(error));
 
 app.listen(PORT, console.log("server running!"));
